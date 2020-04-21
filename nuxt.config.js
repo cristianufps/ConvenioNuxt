1e35bee891128b9a5c8b730ce0ee3cf6cdd25964
Edinson Antolinez <edinson.antolinez@tus-cuentas.com>
2020-04-21 18:44:27 -0500

    try deploy frront

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,99 +1,101 @@
+import axios from 'axios'
 
 export default {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: '~/components/loading.vue',
-  /*
-  ** Global CSS
-  */
-  css: [
-    'ant-design-vue/dist/antd.css',
-    { src: './assets/css/style.css' }
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/antd-ui',
-    '@/plugins/vue-axios',
-    '@/plugins/axios'
-  ],
-  /*  
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-    baseURL: 'http://localhost:4200'
-  },
-  /*
-  ** Login Configuration 
-  */
-  auth: {
-    // Options
-    redirect: {
-      login: '/authentication/login',
-      logout: '/',
-      callback: '/authentication/login',
-      home: '/'
-    },
-    strategies: {
-      local: {
-        endpoints: {
-          login: {
-            url: '/login', method: 'post', propertyName: 'token'
-          },
-          logout: false,
-          user: { url: '/me', method: 'get', propertyName: 'user' }
-        },
-        tokenRequired: true,
-        tokenType: 'Bearer'
-      }
-    },
-    resetOnError: true
-  },
-
-  router: {
-    middleware: ['auth']
-  },
-
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/auth'
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
+    mode: 'universal',
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
+     ** Headers of the page
+     */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: '~/components/loading.vue',
+    /*
+     ** Global CSS
+     */
+    css: [
+        'ant-design-vue/dist/antd.css',
+        { src: './assets/css/style.css' }
+    ],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [
+        '@/plugins/antd-ui',
+        '@/plugins/vue-axios',
+        '@/plugins/axios'
+    ],
+    /*  
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    axios: {
+        baseURL: 'https://convenio-273922.uc.r.appspot.com'
+    },
+    /*
+     ** Login Configuration 
+     */
+    auth: {
+        // Options
+        redirect: {
+            login: '/authentication/login',
+            logout: '/',
+            callback: '/authentication/login',
+            home: '/'
+        },
+        strategies: {
+            local: {
+                endpoints: {
+                    login: {
+                        url: '/login',
+                        method: 'post',
+                        propertyName: 'token'
+                    },
+                    logout: false,
+                    user: { url: '/me', method: 'get', propertyName: 'user' }
+                },
+                tokenRequired: true,
+                tokenType: 'Bearer'
+            }
+        },
+        resetOnError: true
+    },
+
+    router: {
+        middleware: ['auth']
+    },
+
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [
+        // Doc: https://github.com/nuxt-community/eslint-module
+    ],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/auth'
+    ],
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
     }
-  }
 }
